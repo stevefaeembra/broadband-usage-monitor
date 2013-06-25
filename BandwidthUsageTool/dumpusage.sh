b0559c9d78371f8a3ed905418a517dad1b60d0b3
steven <stevendkay@gmail.com>
2013-06-25 13:41:53 +0100

    changed to use date in epoch format for easier handling from subsequent scripts

--- BandwidthUsageTool/dumpusage.sh
+++ BandwidthUsageTool/dumpusage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # writes the total number of bytes received and transmitted over a network interface
 # used as a poor man's bandwidth monitoring utility for those, like me, without all-you-can-eat broadband plans :)
-# writes to ~/bandwidth.log
+# writes to ~/bandwidth.csv
 # if running from cron, remember to make sure that the cron path is set up to find ifconfig, and that the bandwidth.log
 # has permissions set to allow writing from the cron job user
 
@@ -11,7 +11,7 @@ if [ ! -f ~/bandwidth.csv ]
 then
     echo -e "Timestamp,Bytes Downloaded,Bytes Uploaded" >> ~/bandwidth.csv
 fi
-echo -n `date` >> ~/bandwidth.csv
+echo -n `date "+%s"` >> ~/bandwidth.csv
 echo -n -e "," >> ~/bandwidth.csv
 echo -n -e `ifconfig -a $interface | grep "RX bytes" | sed "s/TX.*//" | sed "s/.*[:]//" | sed "s/\s.*//"` >> ~/bandwidth.csv
 echo -n -e "," >> ~/bandwidth.csv
